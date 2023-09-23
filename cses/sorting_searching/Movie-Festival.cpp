f6336a9c6637d77be6314ee6103ae7539401171a
bilguudeiblgd <bilguudei.b8@gmail.com>
2023-09-23 16:41:44 +0200

    Added Movie Festival

--- cses/sorting_searching/Movie-Festival.cpp
+++ cses/sorting_searching/Movie-Festival.cpp
@@ -7,25 +7,27 @@ using namespace std;
  
  
 int main() {
-    // int n;
-    // cin >> n;
+    int n;
+    cin >> n;
  
-    // vector<pair<int, int> > intervals;
+    vector<pair<int, int> > intervals;
     
-    // for(int i = 0; i < n; i++) {
-    //     int start, end;
-    //     cin >> start >> end;
-    //     intervals.push_back({start, i});
-    //     intervals.push_back({end, i});
-    // }
-    // sort(intervals.begin(), intervals.end());
-    // int currPpl = 0;
-    // int maxPpl = 0;
-    // int watching = 0;
-    // for(int i = 0; i < n * 2; i++) {
-        
-    //     currPpl += intervals[i].second;
-    //     maxPpl = max(currPpl, maxPpl);
-    // }
-    // cout << maxPpl << endl;
+    for(int i = 0; i < n; i++) {
+        int start, end;
+        cin >> start >> end;
+        intervals.push_back(make_pair(end,start));
+    }
+    // Sort by ending times
+    sort(intervals.begin(), intervals.end());
+    int ending = 0;
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(ending <= intervals[i].second) {
+            ending = intervals[i].first;
+            count++;
+        }
+    }
+   
+    cout << count << endl;
+
 }
